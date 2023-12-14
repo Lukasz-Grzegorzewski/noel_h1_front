996583a2fbf6a6c76c7bae26ee2ccafb046e4c16
lukasz <zed11temp@gmail.com>
2023-12-14 14:14:24 +0100

    key, cert commented from vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    https: {
-      key: fs.readFileSync('./ssl/key.pem'),
-      cert: fs.readFileSync('./ssl/cert.pem'),
-    }
-  }
+  // server: {
+  //   host: true,
+  //   https: {
+  //     key: fs.readFileSync('./ssl/key.pem'),
+  //     cert: fs.readFileSync('./ssl/cert.pem'),
+  //   }
+  // }
 })
